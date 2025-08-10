8bae7aa95995f128ee416775f897d38ce6ac92be
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-08-10 09:56:23 +0800

    Update 2speak.js

--- 2speak.js
+++ 2speak.js
@@ -26,6 +26,7 @@ const backendURL = 'https://newsbeiter.onrender.com';
 channelList.innerHTML='';
 for (let p of pattern){channelList.innerHTML+=`<button class="btn sepia me-1 mb-1" type="button" onclick="get1stList('lesson','句型 | ${p}','${p}')">${p}</button>`;}
 openChannelList();
+get1stList('lesson','句型','')
 //get1stList(siteName, top+' | '+site[0][1],site[0][0]);
 
 
@@ -38,7 +39,7 @@ uploadBtn.addEventListener('click', async () => {
     try {
       if (navigator.clipboard && navigator.clipboard.readText) {
         textContent = await navigator.clipboard.readText();
-        if (textContent.startsWith('{')){confirmUpload = confirm('是否上傳筆記？');}
+        if (textContent.startsWith('{')){confirmUpload = confirm('是否上傳筆記？');} else {alert('無學習內容可上傳');}
       }
     } catch (err) {
       alert('無學習內容可上傳');
@@ -60,11 +61,16 @@ uploadBtn.addEventListener('click', async () => {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 async function lessonGetList(siteName,t){
+  if(t===''){
   try{url=`${backendURL}/lessons/list?rr=${rr}`;
   const res = await fetch(url);const str = await res.json();
-  for (let h of str){html+=`<p class="title" onclick="getContent('${siteName}',this.id,'${h.path}')">${h.title}</p><div id="${h.path}" class="content" onclick="getContent('${siteName}',this.id,'${h.path}')"></div><hr>`}
+    for (let h of str){html+=`<p class="title" onclick="getContent('${siteName}',this.id,'${h.path}')">${h.title}</p><div id="${h.path}" class="content" onclick="getContent('${siteName}',this.id,'${h.path}')"></div><hr>`}
+
   }catch{html='<p>尚無內容</p>'}
   return html;
+  } else {
+  lessonGetSearchResults(siteName,t);
+  }
 }
 
 async function lessonGetContent(id){
@@ -98,9 +104,31 @@ async function lessonGetSearchResults(siteName,t){
   try{url=`${backendURL}/lessons/search?q=${encodeURIComponent(t)}`;
   let res=await fetch(url);
   let str=await res.json();
-  for (let h of str){
-    html+=`<p class="title" onclick="getContent('${siteName}',this.id,'${h.path}')">${h.title}</p><div id="${h.path}" class="content" onclick="getContent('${siteName}',this.id,'${h.path}')"></div><hr>`;
+  for (let a of str){
+    url=`${backendURL}/notes/read`;
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({category:'2speak', path:a.path})
+    });
+    if (!res.ok) throw new Error('無法讀取筆記');
+    const ss=await res.json();
+    for (let s of ss){
+    if (s) {
+        var h=JSON.parse(s);
+        if(h.tag==t+'句型'){items.push(h);
+    html+=`
+      <p class="fs12 fw-bold" style="color:#4b7bf5">${h.content.en}<br>${h.content.zh}</p>
+      <p>${h.tag}：<span style="color:#4b7bf5">${h.content.pattern}</span><br>${h.content.grammar}</p>
+      <p>${h.content.mistake_to_avoid}</p>`;
+    html+=h.content.extra_examples?`<p class="fs12">${h.content.extra_examples[0]}</p><p class="fs12">${h.content.extra_examples[1]}</p>`:'';
+    html+=`<p class="fs10 fw-normal">${h.src}<br>
+      ${h.content.tone} | ${h.content.register} | ${h.content.cefr}</p>
+      <br><hr>`;
+    }
   }
-  }catch{html='<p>尚無內容</p>'}
+  }
+  }
+  }catch{if(html==''){html='<p>尚無內容</p>'}else{return html}}
   return html;
 }
